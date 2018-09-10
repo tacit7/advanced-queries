60057477b56ee628695ed3296e8851aa0876e56e
uriel <uriel781@gmail.com>
2018-09-09 19:21:16 -0500

    Add timestamps

--- db/migrate/20240605174212_create_db.rb
+++ db/migrate/20240605174212_create_db.rb
@@ -6,20 +6,24 @@ class CreateDb < ActiveRecord::Migration[7.1]
       table.integer :location_id
       table.integer :manager_id
       table.integer :salary, null: false
+      table.timestamps
     end
 
     create_table :roles, force: true do |table|
       table.string :name, null: false
       table.boolean :billable, null: false, default: false
+      table.timestamps
     end
 
     create_table :locations, force: true do |table|
       table.string :name, null: false
       table.integer :region_id, null: false
+      table.timestamps
     end
 
     create_table :regions, force: true do |table|
       table.string :name, null: false
+      table.timestamps
     end
   end
 end
